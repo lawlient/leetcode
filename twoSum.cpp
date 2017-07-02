9c3f67674f79cf0be70cb89510c027d84feece18
lawlient <jiuanx2011@163.com>
2017-07-02 22:02:56 +0800

    redo twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,46 +1,38 @@
+/*  Given an array of integers, return indices of the two numbers such that they add up to a specific target.  
+ *  You may assume that each input would have exactly one solution, and you may not use the same element twice.  
+ *
+ *  Example: 
+ *    Given nums = [2, 7, 11, 15], target = 9, 
+ *    Because nums[0] + nums[1] = 2 + 7 = 9,
+ *    return [0, 1].
+ *
+ *
+*/
+
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
-/*
-vector<int> twoSum(vector<int>& nums, int target) {
-  int i = 0, j = 0;
-  for (auto first = nums.begin(); first != nums.end(); ++first) {
-    if (*first < target) {
-      auto second = find(first + 1, nums.end(), target - *first);
-      if (second != nums.end()) {
-        i = first - nums.begin();
-        j = second - nums.begin();
+
+class Solution {
+public:
+  std::vector<int> twoSum(vector<int>& nums, int target) {
+    std::vector<int> res;
+    auto begin = nums.begin();
+    auto end = nums.end();
+    for (auto b = begin, e = end; b != e; b++) {
+      auto other = std::find(begin, end, target - *b);
+      if (other != end && other != b) {
+        res.push_back(b - begin);
+        res.push_back(other - begin);
         break;
       }
     }
+    return res;
   }
-  return vector<int>{i, j};
-}
-*/
-
-vector<int> twoSum(vector<int>& nums, int target) {
-  vector<int> res;
-  for (auto b = nums.begin(), e = nums.end();
-       b != e; ++b) {
-    auto second = find(b + 1, e, target - *b);
-    if (second != e) {
-      res.push_back(b - nums.begin());
-      res.push_back(second - nums.begin());
-      break;
-    }
-  }
-  return res;
-}
+};
 
 int main() {
-  vector<int> a{3, 2, 4};
-  int target = 6;
-  auto res = twoSum(a, target);
-  for (auto itr : res) {
-    std::cout << itr << "\t";
-  }
-  std::cout << std::endl;
-  return 0;
 }

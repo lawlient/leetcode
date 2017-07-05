31677af8560e0f80e61dcb7294150f7bc4baf9a1
lawlient <jiuanx2011@163.com>
2017-07-05 20:47:22 +0800

    redo longestPalindrome.cpp

--- hard/findMedianSortedArrays.cpp
+++ hard/findMedianSortedArrays.cpp
@@ -13,69 +13,53 @@
  *
 */
 
+
+/*  the key to success is find k'th number
+ *  see quickSort
+ *  partition : x, left < x , right > x
+ *     if k'th < x 
+ *     then find k'th in left 
+ *     else find (k-left) in right
+ *
+ *
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
 
 class Solution {
-  int getKth(const std::vector<int> &v1, int start1,
-             const std::vector<int> &v2, int start2, int k) {
-    int m = v1.size(), n = v2.size();
-    int l1 = m - start1, l2 = n - start2;
-    if (l1 > l2) return getKth(v2, start2, v1, start1, k);
-
-    if (l1 == 0) return v2[start2 + k - 1];
-    if (k == 1) return std::min(v1[start1], v2[start2]);
-
-    int i = std::min(l1, k / 2);
-    int j = std::min(l2, k / 2);
-    if (v1[start1 + i - 1] > v2[start2 + j - 1]) 
-      return getKth(v1, start1, v2, start2 + j, k - j);
-    else
-      return getKth(v1, start1 + i, v2, start2, k - i);
-    return 0;
-  }
-public:
-  double findMedianSortedArrays(std::vector<int> &nums1, std::vector<int> &nums2) {
-    if (nums1.size() > nums2.size()) std::swap(nums1, nums2);
-    int l = (nums1.size() + nums2.size() + 1) >> 1;
-    int r = (nums1.size() + nums2.size() + 2) >> 1;
-    return (getKth(nums1, 0, nums2, 0, l) + getKth(nums1, 0, nums2, 0, r)) / 2.0;
-  }
-
-  double findMedianSortedArrays2(std::vector<int> &nums1, std::vector<int> &nums2) {
-    size_t m = nums1.size();
-    size_t n = nums2.size();
-    if (m > n) { std::swap(m, n); std::swap(nums1, nums2); }
-    size_t imin = 0, imax = m, half_length = (m + n + 1) / 2;
-    double max_of_left = 0, min_of_rigth = 0;
-    while (imin <= imax) {
-      size_t i = (imin + imax) / 2;
-      size_t j = half_length - i;
-      if (i < m && nums1[i] < nums2[j-1]) imin = i + 1;
-      else if (i > 0 && nums1[i-1] > nums2[j]) imax = i - 1;
-      else {
-        if (i == 0) max_of_left = nums2[j-1];
-        else if(j == 0) max_of_left = nums1[i-1];
-        else max_of_left = std::max(nums1[i-1], nums2[j-1]);
-
-        if ((m + n) % 2 == 1) return max_of_left;
-
-        if (i == m) min_of_rigth = nums2[j];
-        else if (j == n) min_of_rigth = nums1[i];
-        else min_of_rigth = std::min(nums1[i], nums2[j]);
-
-        return (max_of_left + min_of_rigth) / 2.0;
+  // find v[b..e] kth number
+  int getKNumber(std::vector<int> &v, int b, int e, int k) {
+    int i = b;
+    for (int j = i+1; j <= e; j++) {
+      if (v[j] <= v[b]) {
+        std::swap(v[++i], v[j]);
       }
     }
-    return 0;
+    std::swap(v[b], v[i]);
+
+    if (i - b + 1 < k)
+      return getKNumber(v, i+1, e, k-i+b-1);
+    else if (i - b + 1 > k)
+      return getKNumber(v, b, i-1, k);
+    else 
+      return v[i];
+  }
+public:
+  double findMedianSortedArraysNew(std::vector<int> &nums1, std::vector<int> &nums2) {
+    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+    auto size = nums1.size();
+    int l = (size + 1) >> 1;
+    int r = (size + 2) >> 1;
+    return (getKNumber(nums1, 0, size - 1, l) + getKNumber(nums1, 0, size - 1, r)) / 2.0;
   }
 };
 
 int main() {
   Solution solute;
   std::vector<int> a{1, 3};
-  std::vector<int> b{2};
-  std::cout << solute.findMedianSortedArrays(a, b) << std::endl;
+  std::vector<int> b{2, 4};
+  std::cout << solute.findMedianSortedArraysNew(a, b) << std::endl;
   return 0;
 }
--- medium/longestPalindrome.cpp
+++ medium/longestPalindrome.cpp
@@ -11,6 +11,15 @@
  *    Output: "bb"
 */
 
+/*  for : 0 -> n
+ *  1. find all same continuous char(jump step)
+ *  2. if left char == right char
+ *     then max + 2 do step 2
+ *     else the string length and the current max length
+ *
+ *
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -20,21 +29,20 @@ class Solution {
 
 public:
   std::string longestPalindrome(std::string s) {
-    size_t length = s.size();
-    if (length < 2) return s;
-    size_t b = 0, max = 1;
-    size_t l = 0, r = 0;
-    for (size_t i = 0; i < length && length - i > max / 2;) {
-      l = r = i;
-      while (r < length - 1 && s[r+1] == s[r]) r++;
+    int size = s.size();
+    if (size < 2) return s;
+    int b = 0, max = 1;
+    for (int i = 0; i < size && size - i > max / 2;) {
+      int l = i, r = i;
+      while (r < size - 1 && s[r+1] == s[r]) r++;
       i = r + 1;
-      while (r < length - 1 && l > 0 && s[l-1] == s[r+1]) {
+      while (r < size - 1 && l > 0 && s[l-1] == s[r+1]) {
         l--;
         r++;
       }
       if (max < r - l + 1) {
-        max = r - l + 1;
         b = l;
+        max = r - l + 1;
       }
     }
     return s.substr(b, max);

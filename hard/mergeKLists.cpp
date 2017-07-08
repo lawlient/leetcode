e846c26e017052da67ae3fddee7fea3f52c7e0c9
lawlient <jiuanx2011@163.com>
2017-07-08 22:11:58 +0800

    redo merge k lists & use priority_queue

--- hard/mergeKLists.cpp
+++ hard/mergeKLists.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <queue>
 
 struct ListNode {
   int val;
@@ -15,6 +16,7 @@ struct ListNode {
 };
 
 class Solution {
+
   ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
     if (l1 == nullptr) return l2;
     if (l2 == nullptr) return l1;
@@ -33,8 +35,30 @@ class Solution {
     if (q) p->next = q;
     return l1;
   }
+
+  struct compare {
+    bool operator()(ListNode *l, ListNode *r) {
+      return l->val > r->val;
+    }
+  };
 public:
   ListNode *mergeKLists(std::vector<ListNode *> &lists) {
+    // auto compare = [](ListNode *l, ListNode *r) { return l->val > r->val; };
+    std::priority_queue<ListNode *, std::vector<ListNode *>, compare> q;
+    for (auto &l : lists)
+      if (l) q.push(l);
+    if (q.empty()) return NULL;
+    auto res = q.top(); q.pop();
+    if (res->next) q.push(res->next);
+    auto r = res;
+    while (!q.empty()) {
+      r = r->next = q.top(); q.pop();
+      if (r->next) q.push(r->next);
+    }
+    return res;
+  }
+
+  ListNode *mergeKLists2(std::vector<ListNode *> &lists) {
     while (lists.size() > 1) {
       auto l1 = lists.back();
       lists.pop_back();

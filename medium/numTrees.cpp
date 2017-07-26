9ba63d4de4da6813039719443129b364a75d123d
lawlient <jiuanx2011@163.com>
2017-07-26 08:36:32 +0800

    calculate iterativly

--- medium/numTrees.cpp
+++ medium/numTrees.cpp
@@ -12,6 +12,11 @@
  *
 */
 
+/* 
+ *
+ *
+*/
+
 #include <iostream>
 #include <climits>
 #include <algorithm>
@@ -33,9 +38,13 @@ public:
     std::vector<int> G(n+1, 0);
     G[0] = G[1] = 1;
 
-    for (int i = 2; i <= n; i++) 
-      for (int j = 1; j <= i; j++)
+    // calculate BST's number as i is root
+    for (int i = 2; i <= n; i++) {
+      // accumulate number from 1 is root to i is root
+      for (int j = 1; j <= i; j++) {
         G[i] += G[j-1] * G[i-j];
+      }
+    }
 
     return G[n];
   }

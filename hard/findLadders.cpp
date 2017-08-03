4a264f3136773cbc354aa0d1e4c5827b06ca54d8
lawlient <jiuanx2011@163.com>
2017-08-03 17:12:15 +0800

    doing find ladders

---
+++ hard/findLadders.cpp
@@ -0,0 +1,100 @@
+/*  Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+ *
+ *  Only one letter can be changed at a time
+ *  Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+ *
+ *  For example,
+ *  
+ *  Given:
+ *  beginWord = "hit"
+ *  endWord = "cog"
+ *  wordList = ["hot","dot","dog","lot","log","cog"]
+ *  
+ *  Return
+ *  
+ *    [
+ *      ["hit","hot","dot","dog","cog"],
+ *      ["hit","hot","lot","log","cog"]
+ *    ]
+ *  
+ *  Note:
+ *  
+ *      Return an empty list if there is no such transformation sequence.
+ *      All words have the same length.
+ *      All words contain only lowercase alphabetic characters.
+ *      You may assume no duplicates in the word list.
+ *      You may assume beginWord and endWord are non-empty and are not the same.
+ *  
+ *
+*/
+
+/* 
+ *  
+ *  
+ *  
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <list>
+
+class Solution {
+  bool isTransformation(const std::string &a, const std::string &b) {
+    int diff = 0;
+    for (int i = 0; i < a.size(); i++) {
+      if (a[i] != b[i]) {
+        if (diff > 1)
+          return false;
+        else 
+          diff++;
+      }
+    }
+    return diff == 1;
+  }
+  
+  void help(std::vector<std::vector<std::string>> &res, std::vector<std::string> &one,
+            std::string beginWord, std::string endWord, std::set<std::string> &wordList) {
+    one.push_back(beginWord);
+    if (isTransformation(beginWord, endWord)) {
+      one.push_back(endWord);
+      res.push_back(one);
+      return;
+    }
+    auto findBegin = std::find(wordList.begin(), wordList.end(), beginWord);
+    if (findBegin != wordList.end()) {
+      wordList.erase(findBegin);
+    }
+    for (auto &w : wordList) {
+      if (isTransformation(w, beginWord)) {
+        help(res, one, w, endWord, wordList);
+      }
+    }
+    if (findBegin != wordList.end()) {
+      wordList.push_back(beginWord);
+    }
+  }
+public:
+  std::vector<std::vector<std::string>> ladderLength(std::string beginWord, std::string endWord, std::vector<std::string> &wordList) {
+    std::vector<std::vector<std::string>> res;
+    if (std::find(wordList.begin(), wordList.end(), endWord) == wordList.end())
+      return res;
+    std::vector<std::string> one;
+    std::set<std::string> words(wordList.begin(), wordList.end());
+    help(res, one, beginWord, endWord, words);
+    return res;
+  }
+};
+
+int main() {
+  return 0;
+}
